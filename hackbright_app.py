2d950d63636784961d21f48c5237cad5f74a6a14
Hackbright Student <user@chromebox-005.(none)>
2013-10-09 17:51:35 -0700

    First commit

--- hackbright_app.py
+++ hackbright_app.py
@@ -3,6 +3,20 @@ import sqlite3
 DB = None
 CONN = None
 
+def make_new_student(first_name, last_name, github):
+    query = """INSERT INTO Students VALUES (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github))
+
+    CONN.commit()
+    print "Successfully added student: %s %s" % (first_name, last_name)
+
+def make_new_project(title, max_grade, description):
+    query = """INSERT INTO Projects VALUES (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+
+    CONN.commit()
+    print "Successfully added project: %s" % (title)
+
 def get_student_by_github(github):
     query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
     DB.execute(query, (github,))
@@ -10,6 +24,16 @@ def get_student_by_github(github):
     print """\
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
+    
+def get_project_title(title):
+    query = """SELECT * FROM Projects WHERE title = ?"""
+    DB.execute(query, (title,))
+    row = DB.fetchone()
+    print """\
+Title: %s
+Description: %s
+Max Grade: %s""" % (row[0], row[1], row[2])
+
 
 def connect_to_db():
     global DB, CONN
@@ -29,6 +53,11 @@ def main():
             get_student_by_github(*args) 
         elif command == "new_student":
             make_new_student(*args)
+        elif command == "project_title":
+            get_project_title(*args)
+        elif command == "new_project":
+            description = " ".join(args[2:])
+            make_new_project(args[0], args[1], description)
 
     CONN.close()
 
